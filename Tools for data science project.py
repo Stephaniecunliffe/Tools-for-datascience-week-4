6d4820c925c5b1f70201837886dc122551e88c2b
Stephaniecunliffe <118996476+Stephaniecunliffe@users.noreply.github.com>
2022-12-06 14:10:30 +0000

    Add files via upload

---
+++ Tools for data science project.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM watson studio
+
+# **Stephanie Cunliffe**
+# 
+# Digital process engineer
+
+# *I am interested in data science because it is the key to unlocking the next industrial revolution. Data science is fascinating because it enables powerful insights using data and tools that we already have available if we apply it correctly*
+
+# ### The code in cell 5 computes my favourite multiplication and displays it as an output
+
+# In[1]:
+
+
+x = 9*7
+print("Stephs favourite number is", x)
+
+
+# ***
+# My favourite animals
+# ***
+# 
+# * <a href="https://a-z-animals.com/blog/the-10-largest-ducks-in-the-world/" target="_blank">Ducks</a>
+# 
+# * <a href="https://www.theguardian.com/science/2019/jun/17/how-dogs-capture-your-heart-evolution-puppy-dog-eyes" target="_blank">Dogs</a>
+# * <a href="https://www.nhm.ac.uk/discover/what-are-dinosaurs.html" target="_blank">Dinosaurs</a>
+# * <s> Dingos </s>
+
+# In[ ]:
+
+
+
+
